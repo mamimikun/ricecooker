4decf65a84a8c9d82f80cdb300db02e154fb4170
garlicnoodles <garlicnoodles@protonmail.com>
2021-07-04 21:08:16 -0500

    added own dwm build

--- progs/git_progs.sh
+++ progs/git_progs.sh
@@ -1,6 +1,6 @@
 GIT_PROGS=(
 
-    'https://git.suckless.org/dwm' # the window manager
+    'https://github.com/mamimikun/dwm' # the window manager
     'https://git.suckless.org/st' # the terminal
     'https://git.suckless.org/dmenu' # the menu
     
